cbae7d9f54087d3be1d420321d46eed5809fa8f4
Vincent Velthuizen <3620651+VincentVelthuizen@users.noreply.github.com>
2018-12-03 11:48:16 +0100

    Roll a ball tutorial (complete +extra)

---
+++ Roll a Ball/Assets/Scripts/WorldController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    /* void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-1 * moveVertical, 0.0f, -1 * moveHorizontal);
+
+        transform.Rotate(movement);
+    } */
+	
+}
